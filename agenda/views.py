c652c63a8753ad05c4593540f139b5877d730a4e
Luiz Guilherme Silva <luizguilhermeprog@gmail.com>
2015-06-16 21:31:44 -0300

    Added form to create events on the page

--- agenda/views.py
+++ agenda/views.py
@@ -1,12 +1,20 @@
 # -*- coding: utf-8 -*-
 
-from django.views.generic import ListView
+from django.views.generic.edit import FormView
 from schedule.models import Event
+from django.contrib.auth.decorators import login_required
+from schedule.forms import EventForm
 
-class MainPage(ListView):
+class MainPage(FormView):
 
-    model = Event
+    form_class = EventForm
     template_name = "base.html"
+    success_url = "/"
+
+    def get_form_kwargs(self):
+        kwargs = super(MainPage, self).get_form_kwargs()
+        kwargs['creation_user'] = self.request.user
+        return kwargs
 
     def get_context_data(self, **kwargs):
         context = super(MainPage, self).get_context_data(**kwargs)
@@ -17,5 +25,9 @@ class MainPage(ListView):
         context['concludeds'] = Event.objects.filter(
             status=Event.CONCLUDED).order_by('-datetime')[:10]
         return context
+
+    def form_valid(self, form):
+        form.save()
+        return super(MainPage, self).form_valid(form)
 
 home = MainPage.as_view()
--- schedule/forms.py
+++ schedule/forms.py
@@ -3,17 +3,18 @@ from schedule.models import Event
 
 class EventForm(forms.ModelForm):
     
-    def __init__(self, *args, **kwrags):
+    def __init__(self, *args, **kwargs):
         self.creation_user = kwargs.pop('creation_user')
         super(EventForm, self).__init__(*args, **kwargs)
+        self.fields['status'].required = False
     
     class Meta:
         model = Event
-        fields = ('name', 'datetime', 'comments', 'status')
+        fields = ('name', 'datetime', 'comment', 'status')
         
     def save(self):
         self.instance = super(EventForm, self).save(commit=False)
-        if not self.instance.creation_user:
+        if not self.instance.id:
             self.instance.creation_user = self.creation_user
         self.instance.save()
         return self.instance
